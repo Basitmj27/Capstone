34fd69b85d84078bf178395e50b95b1b25738c6a
Basit Majeed <116869119+Basitmj27@users.noreply.github.com>
2023-01-02 19:05:53 +0500

    updated version

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
-
-
+import './categories.styles.scss'
+import CategoryItem from './component/category.item.component';
 const App = () => {
+
+  const categories = [
+    {
+      id: 1,
+      tilte: 'Hats',
+      imageUrl:	"https://i.ibb.co/cvpntL1/hats.png",
+    },
+    {
+      id: 2,
+      tilte: 'Jackets',
+      imageUrl:	"https://i.ibb.co/px2tCc3/jackets.png",
+    },
+    {
+      id: 3,
+      tilte: 'Sneakers',
+      imageUrl:	"https://i.ibb.co/0jqHpnp/sneakers.png",
+    },
+    {
+      id: 4,
+      tilte: 'Womens',
+      imageUrl:	"https://i.ibb.co/GCCdy8t/womens.png",
+    },
+    {
+      id: 5,
+      tilte: 'Mens',
+      imageUrl:	"https://i.ibb.co/R70vBrQ/men.png",
+    }
+  ];
   return (
-    <div className="App">
-        Hello World
+    <div className='categories-container'>
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
     </div>
   );
 }
---
+++ src/component/category.item.component.jsx
@@ -0,0 +1,20 @@
+import './category.style.scss'
+
+const categoryItem = ({category}) => {
+    const{imageUrl, tilte} = category;
+    return(
+        <div className="category-container">
+            <div 
+            className="background-image" style={{ 
+            backgroundImage: `url(${imageUrl})`,
+            }}
+            />
+            <div className="category-body-container">
+            <h2>{tilte}</h2>
+            <p>shop Now</p>
+            </div>
+        </div> 
+    );
+}
+
+export default categoryItem
