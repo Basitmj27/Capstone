62baeb3e02ccce0e81d74d2e27e2b16903a43eb0
Basit Majeed <116869119+Basitmj27@users.noreply.github.com>
2023-01-05 17:43:48 +0500

    create signup form and store values in firebase data base

--- src/routes/sign-in/sign-in-component.jsx
+++ src/routes/sign-in/sign-in-component.jsx
@@ -1,14 +1,40 @@
-import {signInWithGooglePopup, createUserDocumentFromAuth} from '../../utils/firebase/firebase.utils';
+import { useEffect } from 'react';
+import { getRedirectResult } from 'firebase/auth';
+import {signInWithGooglePopup, createUserDocumentFromAuth, signInWithGoogleRedirect, auth} from '../../utils/firebase/firebase.utils';
+import { async } from '@firebase/util';
+import SignUpFrom from '../../component/sign-up/sign-up-form.component';
+
+
 const SignIn = () => {
+
+    /*useEffect (async () => {
+        const response = await getRedirectResult(auth);
+        if (response) {
+            const userDocRef = await createUserDocumentFromAuth(response.user);
+        }
+    }, []);*/
+
+    useEffect(() => {
+        async function fetchData() {
+            const response = await getRedirectResult(auth);
+            if (response) {
+                const userDocRef = await createUserDocumentFromAuth(response.user);
+            }
+        }
+        fetchData();
+    }, []);
+
     const logGoogleUser = async () => {
         const {user} = await signInWithGooglePopup();
-       createUserDocumentFromAuth(user);
-    }
+        const userDocRef = await createUserDocumentFromAuth(user);
+    };
 
+    
     return (
         <div>
             <h1>Sign In Page</h1>
             <button onClick={logGoogleUser}>Sign In With Google Popup</button>
+           <SignUpFrom />
         </div>
     );
 }
