781bb8b64b11720e86c612ebff612f3ee3da50e6
Basit Majeed <116869119+Basitmj27@users.noreply.github.com>
2023-01-19 17:45:55 +0500

    add saga library and modify code

---
+++ src/store/user/user.saga.js
@@ -0,0 +1,35 @@
+import { takeLatest, all, call, put } from 'redux-saga/effects';
+
+import { USER_ACTION_TYPES } from './user.types';
+
+import { signInSuccess, signInFailed } from './user.action';
+
+import { getCurrentUser, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
+
+export function* getSnapshotFromUserAuth(userAuth, additionalDetail) {
+    try{
+        const userSnapshot = yield call(createUserDocumentFromAuth, userAuth, additionalDetail);
+        console.log(userSnapshot);
+        console.log(userSnapshot.data());
+    } catch (error) {
+        yield put(signInFailed(error));
+    }
+}
+
+export function* isUserAuthenticated() {
+    try{
+        const userAuth = yield call(getCurrentUser);
+        if(!userAuth) return;
+        yield call(getSnapshotFromUserAuth, userAuth);
+    } catch (error) {
+        yield put(signInFailed(error));
+    }
+}
+
+export function* onCheckUserSesion() {
+    yield takeLatest(USER_ACTION_TYPES.CHECK_USER_SESSION, isUserAuthenticated);
+}
+
+export function* userSagas() {
+    yield all([call(onCheckUserSesion)]);
+}
